8d5ede53c1234ab51186068e8e45f65eb8559de4
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-02-09 09:25:06 -0800

    Feat: Setup basic form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+  }
+
+  handleChange = (event) =>
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  handleSubmit = () => {
+    // no-op for example
+  }
+
   render() {
     return (
       <div className="App">
-        form
+        <form onSubmit={this.handleSubmit}>
+          <label>We love mail ✨</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="mail"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            name="message"
+            type="text"
+            placeholder="message"
+            value={this.state.message}
+            onChange={this.handleChange}
+          />
+          <input
+            type="submit"
+            value="SEND"
+          />
+        </form>
       </div>
     );
   }
